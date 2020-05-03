c583efa128c14c680a419c1a3ecf274163badf62
Marcio Ramos <marcioalvesramos@hotmail.com>
2020-05-03 18:47:01 -0300

    Add transaction custom repository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,30 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = this.find();
+
+    const income = (await transactions)
+      .reduce((prev: number, transaction: Transaction) =>{
+
+        return transaction.type === 'income' ?
+          prev + Number(transaction.value) :
+          prev},
+        0.00);
+
+    const outcome = (await transactions)
+      .reduce((prev: number, transaction: Transaction) =>
+        transaction.type === 'outcome' ?
+          prev + Number(transaction.value) :
+          prev,
+        0.00);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    }
   }
 }
 
